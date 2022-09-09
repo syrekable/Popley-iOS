6a7909143c260f814d582bd568e188200bde0a16
Jordan Niedzielski <contact@syrekable.tech>
2022-09-09 11:24:12 +0200

    Initial Commit

---
+++ Popley/PopleyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopleyApp.swift
+//  Popley
+//
+//  Created by Jordan Niedzielski on 09/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct PopleyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
