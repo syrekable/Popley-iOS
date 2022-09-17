fef815952f569f3a3a3a3ff16a2912e7872e1b3b
Jordan Niedzielski <contact@syrekable.tech>
2022-09-17 20:37:12 +0200

    tweaking DetailView

--- Popley/View/PlantDetails.swift
+++ Popley/View/PlantDetails.swift
@@ -10,9 +10,29 @@ import SwiftUI
 struct PlantDetails: View {
     let plant: Plant
     var body: some View {
-        VStack {
-            Text(plant.name)
+        VStack(alignment: .leading, spacing: 15) {
+            Image(plant.picture)
+                .resizable()
+                .scaledToFill()
+                .frame(height: 300)
+                .mask {
+                    Rectangle()
+                }
+                .cornerRadius(10)
+            Text("Needs water every ") +
+            Text(String(describing: plant.waterInterval))
+                .fontWeight(.semibold)
+            Text("Next watering in ") +
+            Text(String(describing: plant
+                .timeToWater
+                .asDescriptiveDateInterval
+            ))
+                .fontWeight(.bold)
+            Spacer()
         }
+        .navigationTitle(plant.name)
+        .padding()
+        
     }
 }
 
