ce1c44a93f437c0fd934cc80462193665244a14f
Jordan Niedzielski <contact@syrekable.tech>
2022-09-23 16:56:59 +0200

    frequency picker from health ported

--- Popley/View/AddPlant/WaterIntervalPickerView.swift
+++ Popley/View/AddPlant/WaterIntervalPickerView.swift
@@ -9,11 +9,11 @@ import SwiftUI
 
 struct WaterIntervalPickerView: View {
     @EnvironmentObject var model: Model
-    @State private var days: Int = 1
-    @State private var lastWatered: Date = Date()
+    @Binding var days: Int
+    @Binding var lastWatered: Date
     
     var body: some View {
-        VStack {
+        VStack(spacing: 30) {
             VStack {
                 HStack {
                     Text("Every")
@@ -21,6 +21,7 @@ struct WaterIntervalPickerView: View {
                     Text("\(days) \(days == 1 ? "day" : "days")")
                         .foregroundColor(.accentColor)
                 }
+                Divider()
                 Picker("Watering frequency in days", selection: $days) {
                     ForEach(1...90, id: \.self) { i in
                         Text("\(i) \(i == 1 ? "day" : "days")").tag(i)
@@ -29,13 +30,22 @@ struct WaterIntervalPickerView: View {
                 .pickerStyle(.wheel)
             }
             .padding()
+            .background(Color("Primary"))
+            .cornerRadius(10)
+            DatePicker("Last watered", selection: $lastWatered, displayedComponents: [.date])
+            // TODO: their vertical padding is half the horizontal
+                .padding()
+                .background(Color("Primary"))
+                .cornerRadius(10)
         }
+        .padding()
     }
 }
 
 struct WaterIntervalPickerView_Previews: PreviewProvider {
     static var previews: some View {
-        WaterIntervalPickerView()
+        WaterIntervalPickerView(days: .constant(1), lastWatered: .constant(Date()))
             .environmentObject(Model())
+            .previewLayout(.sizeThatFits)
     }
 }
