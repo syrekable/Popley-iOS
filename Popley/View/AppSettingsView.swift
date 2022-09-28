3c910eb39485da9ae5ea640736262382b5790045
Jordan Niedzielski <contact@syrekable.tech>
2022-09-28 15:08:20 +0200

    AppSettingsView refactor

--- Popley/View/AppSettingsView.swift
+++ Popley/View/AppSettingsView.swift
@@ -12,42 +12,49 @@ struct AppSettingsView: View {
     @State private var isExactTimeShown: Bool = false
     var body: some View {
         Form {
-            Section {
-                Picker("Time of notification", selection: $time) {
-                    ForEach(NotificationSettings.TimeOfNotification.allCases) { e in
-                        Text(e.rawValue)
-                    }
-                }
-                .disabled(isExactTimeShown)
-            } header: {
-                Text("Notification delivery")
-            } footer: {
-                VStack(alignment: .leading, spacing: 5) {
-                    Text("Conveniently set the preffered reminder delivery time.")
-                    Text("Morning defaults to an hour before Work focus is enabled. Evening defaults to an hour after Work focus is enabled.")
-                    Text("In case Work focus mode is not set up on a periodic basis, Morning defaults to 8 AM and Evening defaults to 6 PM.")
-                }
-            }
-            Section {
-                Toggle("Remind at a different time", isOn: $isExactTimeShown)
-                if isExactTimeShown {
-                    DatePicker(selection: .constant(Date()), displayedComponents: .hourAndMinute) {
-                        Text("Notification time")
-                    }
-                }
-            } header: {
-                Text("Advanced")
-            } footer: {
-                if !isExactTimeShown {
-                    Text("Set precise time for the reminder delivery.")
-                }
-            }
+            notificationDelivery
+            advanced
         }
         .navigationTitle("App Settings")
     }
 }
 
-
+extension AppSettingsView {
+    var notificationDelivery: some View {
+        Section {
+            Picker("Time of notification", selection: $time) {
+                ForEach(NotificationSettings.TimeOfNotification.allCases) { e in
+                    Text(e.rawValue)
+                }
+            }
+            .disabled(isExactTimeShown)
+        } header: {
+            Text("Notification delivery")
+        } footer: {
+            VStack(alignment: .leading, spacing: 5) {
+                Text("Conveniently set the preffered reminder delivery time.")
+                Text("Morning defaults to an hour before Work focus is enabled. Evening defaults to an hour after Work focus is enabled.")
+                Text("In case Work focus mode is not set up on a periodic basis, Morning defaults to 8 AM and Evening defaults to 6 PM.")
+            }
+        }
+    }
+    var advanced: some View {
+        Section {
+            Toggle("Remind at a different time", isOn: $isExactTimeShown)
+            if isExactTimeShown {
+                DatePicker(selection: .constant(Date()), displayedComponents: .hourAndMinute) {
+                    Text("Notification time")
+                }
+            }
+        } header: {
+            Text("Advanced")
+        } footer: {
+            if !isExactTimeShown {
+                Text("Set precise time for the reminder delivery.")
+            }
+        }
+    }
+}
 
 struct AppSettingsView_Previews: PreviewProvider {
     static var previews: some View {
