8549d557e11692667f98b24223904669b967942d
Jordan Niedzielski <contact@syrekable.tech>
2022-09-30 17:37:28 +0200

    preliminary UI reflecting authorization or lack thereof

--- Popley/View/AddPlant/AddPlantSummaryView.swift
+++ Popley/View/AddPlant/AddPlantSummaryView.swift
@@ -16,49 +16,72 @@ struct AddPlantSummaryView: View {
     let lastWatered: Date
     
     var body: some View {
-        VStack(spacing: 30) {
-            if let plant = plant {
-                HStack {
-                    Image(uiImage: image)
-                        .resizable()
-                        .scaledToFit()
-                        .mask {
-                            Circle()
-                        }
-                        .frame(height: 125)
+        ZStack {
+            VStack(spacing: 30) {
+                if let plant = plant {
+                    HStack {
+                        Image(uiImage: image)
+                            .resizable()
+                            .scaledToFit()
+                            .mask {
+                                Circle()
+                            }
+                            .frame(height: 125)
+                        Spacer()
+                        Text(name)
+                            .font(.title)
+                            .fontWeight(.medium)
+                            .frame(maxWidth: 200, alignment: .center)
+                    }
+                    Divider()
+                    // TODO: 38 days -> "~ 1 month" or "> 1 month" instead of "1 month"?
+                    WaterIntervalLabel(description: "Needs water every:", iconName: "calendar.circle", intervalDescription: String(describing: plant.waterInterval))
+                    WaterIntervalLabel(description: "Next watering in:", iconName: "timer", intervalDescription: String(describing: plant
+                        .timeToWater
+                        .asDescriptiveDateInterval
+                    ))
                     Spacer()
-                    Text(name)
-                        .font(.title)
-                        .fontWeight(.medium)
-                        .frame(maxWidth: 200, alignment: .center)
+                    addPlantButton
+                } else {
+                    ProgressView()
                 }
-                Divider()
-                // TODO: 38 days -> "~ 1 month" or "> 1 month" instead of "1 month"?
-                WaterIntervalLabel(description: "Needs water every:", iconName: "calendar.circle", intervalDescription: String(describing: plant.waterInterval))
-                WaterIntervalLabel(description: "Next watering in:", iconName: "timer", intervalDescription: String(describing: plant
-                    .timeToWater
-                    .asDescriptiveDateInterval
-                ))
-                Spacer()
-                Button {
-                    model.addPlant(plant)
-                    model.navigateToRoot()
-                } label: {
-                    ButtonLabel(description: "Add this plant", systemName: "hand.thumbsup.circle")
-                }
-                .disabled(!model.isNotificationAuthorized)
-                .opacity(model.isNotificationAuthorized ? 1 : 0.6)
-            } else {
-                ProgressView()
+            }
+            .overlay(.ultraThinMaterial.opacity(0.7))
+            if !model.isNotificationAuthorized {
+                Color("Secondary").opacity(0.4)
+                    .cornerRadius(15)
+                requestAuthorizationMessage
+                    .padding()
             }
         }
-            .navigationTitle("New plant summary")
-            .padding()// [.horizontal, .bottom]?
-            .onAppear {
-                plant = model.createPlant(named: name, withPicture: image, wateredEvery: wateredEvery, lastWatered: lastWatered)
-                model.requestAuthorization()
-            }
-            
+        .navigationTitle("New plant summary")
+        .padding()
+        .onAppear {
+            plant = model.createPlant(named: name, withPicture: image, wateredEvery: wateredEvery, lastWatered: lastWatered)
+            model.requestAuthorization()
+        }
+    }
+}
+
+extension AddPlantSummaryView {
+    var requestAuthorizationMessage: some View {
+        VStack(alignment: .leading, spacing: 15) {
+            Text("Let \(plant?.name ?? "this plant") flourish")
+                .font(.title)
+                .bold()
+            Text("Let Popley notify you when to water \(plant?.name ?? "this") and other plants. Please, enable notification permission for Popley in the Settings app.")
+                .fontWeight(.semibold)
+        }.foregroundColor(.accentColor)
+    }
+    var addPlantButton: some View {
+        Button {
+            model.addPlant(plant!)
+            model.navigateToRoot()
+        } label: {
+            ButtonLabel(description: "Add this plant", systemName: "hand.thumbsup.circle")
+        }
+        .disabled(!model.isNotificationAuthorized)
+        .opacity(model.isNotificationAuthorized ? 1 : 0.6)
     }
 }
 
