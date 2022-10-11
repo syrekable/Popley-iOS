21b11204bfeed5667162cc307f64cf408a86685f
Jordan Niedzielski <contact@syrekable.tech>
2022-10-11 16:08:05 +0200

    dismiss the welcome screen on button click

--- Popley/View/Welcome/ExploreSettingsView.swift
+++ Popley/View/Welcome/ExploreSettingsView.swift
@@ -8,12 +8,12 @@
 import SwiftUI
 
 struct ExploreSettingsView: View {
+    @EnvironmentObject var model: Model
     var body: some View {
         VStack {
             Text("Now set the notification time that suits you best.")
-            
             Button {
-                
+                model.didLaunchBefore = true
             } label: {
                 ButtonLabel(description: "Open settings", systemName: "gear")
             }
